f627b457176e830b5d666e4a0cd3b282b1390f11
Patrick <synapse.and.syntax@gmail.com>
2013-06-06 15:19:10 -0400

    removed ini problems from main directory

--- ini2/ini2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-if __name__ == "__main__":
-    filename = sys.argv[1]
-    with open(filename) as f:
-        line = f.readline()
-        a,b = map(int,line.split(" "))
-    print a**2 + b**2
