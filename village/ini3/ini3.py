981d7f049be8a376184bd4f315fa3a84ebfd3d96
Patrick <synapse.and.syntax@gmail.com>
2013-06-14 12:25:16 -0400

    problem statement

--- village/ini3/ini3.py
+++ village/ini3/ini3.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+"""
+Problem
+
+Given: A string s of length at most 200 letters and four integers a, b, c and d.
+
+Return: The slice of this string from indices a through b and c
+through d (with space in between), inclusively.  Sample Dataset
+
+HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain.
+22 27 97 102
+
+Sample Output
+
+Humpty Dumpty
+
+"""
+
+
 import sys
 
 if __name__ == "__main__":
