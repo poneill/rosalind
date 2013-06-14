f9bbe574876434ab62bdb12e8e4b539849bad4f4
Patrick <synapse.and.syntax@gmail.com>
2013-06-14 12:23:51 -0400

    added problem statement

--- village/ini1/ini1.py
+++ village/ini1/ini1.py
@@ -1,1 +1,6 @@
+"""
+Problem
+
+After downloading and installing Python, type import this into the Python command line and see what happens. Then, click the "Download dataset" button below and copy the Zen of Python into the space provided.
+"""
 import this
