c2ea1d5455d479f285f1cd92d3c52c1e2bb491dc
Patrick <synapse.and.syntax@gmail.com>
2013-06-14 12:31:22 -0400

    problem statement

--- hamm/hamm.py
+++ hamm/hamm.py
@@ -3,7 +3,20 @@ import sys
 sys.path.append("..")
 from rosalind_utils import *
 
+"""
+Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+
+Return: The Hamming distance dH(s,t).
+Sample Dataset
+
+GAGCCTACTAACGGGAT
+CATCGTAATGACGGCCT
+
+Sample Output
+
+7
+"""
 
 if __name__ == "__main__":
     s,t = get_input()
-    print hamming(s,t)
+    print hamming(s,t) 
